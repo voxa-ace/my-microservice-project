4f733cd9acffe5abec9e317f547232288179093c
Volodymyr Kashnikov <vkashnikov19@gmail.com>
2025-07-07 21:11:28 +0300

    Updated to lesson-7 names and paths

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # terraform {
 #   backend "s3" {
 #     bucket         = "terraform-state-volodymyr-2906"
-#     key            = "lesson-5/terraform.tfstate"
+#     key            = "lesson-7/terraform.tfstate"
 #     region         = "us-east-1"
 #     dynamodb_table = "terraform-locks-2906"
 #     encrypt        = true
--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ module "vpc" {
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  vpc_name           = "lesson-5-vpc"
+  vpc_name           = "lesson-7-vpc"
 }
 
 module "ecr" {
   source       = "./modules/ecr"
-  ecr_name     = "lesson-5-ecr"
+  ecr_name     = "lesson-7-ecr"
   scan_on_push = true
 }
 
